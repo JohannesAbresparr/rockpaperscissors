82f63284b4978fc2f6df82c72b858d7b30dc29e0
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-03-28 17:59:15 +0000

    change style in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,13 @@
  * and possible choices
  */
 
+const choises = ["rock", "paper", "scissors"];
 const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementById("player-score");
 const opponentScore = document.getElementById("opponent-score");
 const playerImage = document.getElementById("player-image");
 const opponentImage = document.getElementById("opponent-image");
 const messages = document.getElementById("messages");
-const choises = ["rock", "paper", "scissors"];
-
 
 /**
  * Event listeners for buttons
@@ -19,31 +18,22 @@ const choises = ["rock", "paper", "scissors"];
 
 for (let button of buttons) {
     button.addEventListener("click", function () {
-        let playerChoise = this.getAttribute("choise");
+        let playerChoise = this.getAttribute("data-choise");
         playGame(playerChoise);
     });
-
-
 }
-
 
 /**
  *Game function, selected button value
  */
 
-
 function playGame(playerChoice) {
 
     playerImage.src = `assets/images/${choises[playerChoise]}.png`;
     playerImage.alt = choises[playerChoise];
-    
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
 
     let opponentChoise = Math.floor(Math.random() * 3);
    
-    opponentImage.src = `assets/images/${choises[playerChoise]}.png`;
-    opponentImage.alt = choises[opponentChoise];
     opponentImage.src = `assets/images/${choices[opponentChoice]}.png`;
     opponentImage.alt = choices[opponentChoice];
 
