f64e70ac941b4a78c02e928fe9fc94e4a85d05dc
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-04-01 19:39:28 +0000

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,29 @@ const game = () => {
     let oScore = 0;
 
 
-    const playGame = () => {
+    function playGame() {
         const options = document.querySelectorAll('.options button');
         const playerChoice = document.querySelector('.playerchoice');
         const opponentChoice = document.querySelector('.opponentchoice');
 
         //**Calculate random opponent choice*/
-
         const opponentOptions = ['rock', 'paper', 'scissors'];
 
         options.forEach(option => {
             option.addEventListener('click', function () {
                 const opponentRandom = Math.floor(Math.random() * 3);
                 const opponentChoice = opponentOptions[opponentRandom];
-                
-                compareChoises(this.textContent, opponentChoice );
+
+                compareChoises(this.textContent, opponentChoice);
 
 
-            //Change image pending choice
-            playerChoice.src = `./assets/images${this.textContent}.png`;
-            opponentChoice.src = `./assets/images${opponentChoice}.png`;
+                //Change image pending choice
+                playerChoice.src = `./assets/images${this.textContent}.png`;
+                opponentChoice.src = `./assets/images${opponentChoice}.png`;
 
             });
         });
-    };
+    }
 
 const updateScore = () => {
     const playerScore = document.querySelector('.player-score p');
