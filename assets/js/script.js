dc8ecb4832956b2b0844cf92e1f1836699a08241
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-04-01 18:05:07 +0000

    add randomised choise for opponent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-const 
+const game = () => {
+    let playerScore = 0;
+    let opponentScore = 0;
+
+const playGame = () => {    
+    const options = document.querySelectorAll('.options button');
+    const playerchoice = document.querySelector('.playerchoice');
+    const opponentchoice = document.querySelector('.opponentchoice');
+
+//**Calculate random opponent choice*/
+
+    const opponentOptions = ['rock','paper','scissors'];
+        
+    options.forEach(option => {
+        option.addEventListener('click', function() {
+            const opponentRandom = Math.floor( Math.random() * 3);
+            const opponentchoice = opponentOptions[opponentRandom];
+            console.log(opponentchoice);
+        });
+
+    });
+
+
+    
+   
+};
+
+
+
+
+}
