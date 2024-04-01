598744d66c5b9b82645dfeb563c8b09db95e3a0d
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-04-01 19:05:29 +0000

    Add function to calculate and update scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,86 @@
 const game = () => {
-    let playerScore = 0;
-    let opponentScore = 0;
+    let pScore = 0;
+    let oScore = 0;
 
-const playGame = () => {    
-    const options = document.querySelectorAll('.options button');
-    const playerchoice = document.querySelector('.playerchoice');
-    const opponentchoice = document.querySelector('.opponentchoice');
+    const playGame = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerChoice = document.querySelector('.playerchoice');
+        const opponentChoice = document.querySelector('.opponentchoice');
 
-//**Calculate random opponent choice*/
+        //**Calculate random opponent choice*/
 
-    const opponentOptions = ['rock','paper','scissors'];
-        
-    options.forEach(option => {
-        option.addEventListener('click', function() {
-            const opponentRandom = Math.floor( Math.random() * 3);
-            const opponentchoice = opponentOptions[opponentRandom];
-            console.log(opponentchoice);
+        const opponentOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function () {
+                const opponentRandom = Math.floor(Math.random() * 3);
+                const opponentChoice = opponentOptions[opponentRandom];
+                
+                compareChoises(this.textContent, opponentChoice );
+
+
+            //Change image pending choice
+            playerChoice.src = `./assets/images${this.textContent}.png`;
+            opponentChoice.src = `./assets/images${opponentChoice}.png`;
+
+            });
         });
+    };
 
-    });
-
-
-    
-   
+const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const opponentScore = document.querySelector('.opponent-score p');
+    playerScore.textContent = pScore;
+    opponentScore.textContent = oScore;
 };
 
+
+    const compareChoises = (playerChoice, opponentChoice) => {
+        const message = document.querySelector('.message');
+        if (playerChoice === opponentChoice){
+            message.textContent = "It's draw!";
+            return;
+        }
+
+        if(playerChoice === 'rock'){
+            if(opponentChoice === 'scissors'){
+                message.textContent = "You won!";
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                message.textContent = "You lost!";
+                oScore++;
+                updateScore();
+                return;
+            }
+        }
+
+        if(playerChoice === 'paper'){
+            if(opponentChoice === 'scissors'){
+                message.textContent = "You lost!";
+                oScore++; updateScore();
+                return;
+            }else{
+                message.textContent = "You won!";
+                pScore++; updateScore();
+                return;
+            }
+        }
+
+        if(playerChoice === 'scissors'){
+            if(opponentChoice === 'rock'){
+                message.textContent = "You lost!";
+                oScore++; updateScore();
+                return;
+            }else{
+                message.textContent = "You won!";
+                pScore++; updateScore();
+                return;
+            }
+        }
+
+    }
 
 
 
